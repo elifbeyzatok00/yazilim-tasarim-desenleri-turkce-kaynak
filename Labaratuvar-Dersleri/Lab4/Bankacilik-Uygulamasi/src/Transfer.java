99fa97de92bbb65e339f24b37293c52b37b2a3bf
elifbeyzatok00 <elifbeyzatok@gmail.com>
2023-11-25 00:22:40 +0300

    commit new changes

--- Labaratuvar-Dersleri/Lab4/Bankacilik-Uygulamasi/src/Transfer.java
+++ Labaratuvar-Dersleri/Lab4/Bankacilik-Uygulamasi/src/Transfer.java
@@ -9,9 +9,12 @@ public class Transfer {
         }
     }
     public void eft(Kullanici gonderici, Kullanici alici, double miktar){
-        double maliyet = Ozel.TL_MALIYET;
+        double maliyet = Ozel.TL_MALIYET; // özel kullanıcı için 20 TL ücret alacak. Bunu diretk verdik
+        //Standart kullanıcı için ise miktarın %5'i kadar ücret alacak. Bunu aşağıdaki is bloğunda hesaplayacağız
         if(gonderici instanceof Standart){
-            double maliyetOran = miktar / Standart.ORAN;
+            double maliyetOran = miktar / Standart.ORAN; // standart kullanıcı için %5 ücret alacak
+            // 5/100 = 1/20 Yani ORAN=20 => 1/ORAN = 1/20 = 0.05 ile
+            // miktar / ORAN = miktar * 1/ORAN = miktar * 0.05 sağladık
             if(maliyetOran > Ozel.TL_MALIYET){
                 maliyet = maliyetOran;
             }
@@ -25,7 +28,7 @@ public class Transfer {
             System.out.println("Yetersiz bakiye");
         }
     }
-    public void swift(Ozel gonderici, Ozel alici, double miktar){
+    public void swift(Ozel gonderici, Ozel alici, double miktar){ // tipi Ozel verdik çünkü swift sadece Ozel kullanıcılar arasında olabilir
 
         if(gonderici.getDovizBakiye() > miktar && gonderici.getTlBakiye() > Ozel.DOVIZ_MALIYET){
             gonderici.setDovizBakiye(gonderici.getDovizBakiye() - miktar);
